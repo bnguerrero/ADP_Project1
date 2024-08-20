4f1c753bfe49e20b5666a9397640129dd1c2b4ae
bnguerrero <bnguerrero@cpp.edu>
2024-08-20 20:36:32 +0000

    changes to CustomerController.java

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Customer;
-import com.example.demo.repository.CustomerRepository;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api")
 public class CustomerController{
 	
 	private List<Customer> customers = Arrays.asList(
@@ -30,7 +27,7 @@ public class CustomerController{
 		return null;
 	}
 	
-	@GetMapping
+	@GetMapping("/customers")
 	public List<Customer>getAllCustomers(){
 		return customers;
 	}
