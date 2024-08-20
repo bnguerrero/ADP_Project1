7ad1aa51f1399b9ee5b1c6452c11224eea79b997
bnguerrero <bnguerrero@cpp.edu>
2024-08-20 22:16:32 +0000

    first commit to phase3

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('bri', '001', 'bri@adp.com'); 
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('chris', '002', 'chris@adp.com'); 
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('kevin', '003', 'kevin@adp.com’);
